11033bfecfec64233da650ecfcfaf10bfb4d8c4c
jason-xy <1462357003@qq.com>
2021-05-11 20:37:52 +0800

    update

--- Code/miniprogram/app.js
+++ Code/miniprogram/app.js
@@ -1,5 +1,13 @@
 App({
+
+  globalData: {
+    wxUserInfo: {},
+  },
+
   onLaunch: function () {
+
+    var that = this
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -7,7 +15,38 @@ App({
         traceUser: true,
       })
     }
+    //云环境初始化
+    wx.cloud.init({
+      env: 'cloud1-5gjnayyifcd4e2a2'
+    })
 
-    this.globalData = {}
+    //获取用户信息
+    wx.showModal({
+      title: '温馨提示',
+      content: '正在请求您的个人信息',
+      success(res) {
+        if (res.confirm) {
+          wx.getUserProfile({
+          desc: "获取你的昵称、头像、地区及性别",
+          success: res => {
+            console.log(res)
+            that.globalData.wxUserInfo = res.userInfo;
+          },
+          fail: res => {
+             //拒绝授权
+            that.showErrorModal('您拒绝了请求');
+            return;
+          }
+        })} else if (res.cancel) {
+          //拒绝授权 showErrorModal是自定义的提示
+          that.showErrorModal('您拒绝了请求');
+          return;
+        }
+      }
+    })
+
+    
+    console.log(this.globalData)
+  
   }
 })

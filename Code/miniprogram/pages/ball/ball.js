77b98f74f2f55ec00ce727b77e4c52c94582496c
FGoucge <83569948+FGoucge@users.noreply.github.com>
2021-05-13 15:16:05 +0800

    Update ball.js

--- Code/miniprogram/pages/ball/ball.js
+++ Code/miniprogram/pages/ball/ball.js
@@ -1,66 +1,155 @@
-// pages/basketball/basketball.js
+const app = getApp();
+const db = wx.cloud.database();
+const cont = db.collection('river_data');
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data: {  
+    basics: 0,
+    num: 0,
+    scroll: 0,
+    data:[] 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  basicsSteps() {
+    this.setData({
+      basics: this.data.basics == this.data.basicsList.length - 1 ? 0 : this.data.basics + 1
+    })
+  },
+  numSteps() {
+    this.setData({
+      num: this.data.num == this.data.numList.length - 1 ? 0 : this.data.num + 1
+    })
+  },
+  scrollSteps() {
+    this.setData({
+      scroll: this.data.scroll == 10 ? 0 : this.data.scroll + 1
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onLoad1: function(event){
+    let taht = this 
+    db.collection('activity').where({
+      type:'篮球'
+    }).get({
+      success: function(res) {
+        console.log('获取成功',res)
+        taht.setData({
+          data:res.data
+        })
+        console.log(that.data.data)
+      },fail:function(res){
+        console.log('获取失败',res)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onLoad2: function(event){
+    let taht = this 
+    db.collection('activity').where({
+      type:'网球'
+    }).get({
+      success: function(res) {
+        console.log('获取成功',res)
+        taht.setData({
+          data:res.data
+        })
+        console.log(that.data.data)
+      },fail:function(res){
+        console.log('获取失败',res)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onLoad3: function(event){
+    let taht = this 
+    db.collection('activity').where({
+      type:'乒乓球'
+    }).get({ 
+      success: function(res) {
+        console.log('获取成功',res)
+        taht.setData({
+          data:res.data
+        })
+        console.log(that.data.data)
+      },fail:function(res){
+        console.log('获取失败',res)
+      }
+    })
+  },
+  
+  onLoad4: function(event){
+    let taht = this 
+    db.collection('activity').where({
+      type:'排球'
+    }).get({
+      success: function(res) {
+        console.log('获取成功',res)
+        taht.setData({
+          data:res.data
+        })
+        console.log(that.data.data)
+      },fail:function(res){
+        console.log('获取失败',res)
+      }
+    })
+  },
+  
+  onLoad5: function(event){
+    let taht = this 
+    db.collection('activity').where({
+      type:'羽毛球'
+    }).get({
+      success: function(res) {
+        console.log('获取成功',res)
+        taht.setData({
+          data:res.data
+        })
+        console.log(that.data.data)
+      },fail:function(res){
+        console.log('获取失败',res)
+      }
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  onLoad6: function(event){
+    let taht = this 
+    db.collection('activity').where({
+      type:'足球'
+    }).get({
+      success: function(res) {
+        console.log('获取成功',res)
+        taht.setData({
+          data:res.data
+        })
+        console.log(that.data.data)
+      },fail:function(res){
+        console.log('获取失败',res)
+      }
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  ChooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let values = e.currentTarget.dataset.value;
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      if (items[i].value == values) {
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+    this.setData({
+      checkbox: items
+    })
   }
 })

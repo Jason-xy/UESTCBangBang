2183b7dbac7f4b07fceaee1da351b055546d0374
FGoucge <83569948+FGoucge@users.noreply.github.com>
2021-05-14 17:05:39 +0800

    Add files via upload

---
+++ Code/miniprogram/pages/release/release.js
@@ -0,0 +1,11 @@
+Page({
+  gotoItemRelease: function(){ 
+    wx.navigateTo({ url: '/pages/item release/item release',
+  })
+   }, 
+ 
+  gotoActivityRelease: function(){ 
+    wx.navigateTo({ url: '/pages/activity release/activity release', 
+  }) 
+  },
+})

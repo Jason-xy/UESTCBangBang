04f9c32314010f71cb49e9ea852c9f73b5e31e98
jason-xy <1462357003@qq.com>
2021-05-14 22:37:47 +0800

    Upload

--- Code/miniprogram/pages/release/release.js
+++ Code/miniprogram/pages/release/release.js
@@ -1,11 +1,11 @@
 Page({
   gotoItemRelease: function(){ 
-    wx.navigateTo({ url: '/pages/item release/item release',
+    wx.navigateTo({ url: '/pages/release/item release/item release',
   })
    }, 
  
   gotoActivityRelease: function(){ 
-    wx.navigateTo({ url: '/pages/activity release/activity release', 
+    wx.navigateTo({ url: '/pages/release/activity release/activity release', 
   }) 
   },
 })
